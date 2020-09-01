6a652e947fac1304e302d66a9bc26c015d902300
George Pemberton <georgepemberton1998@gmail.com>
2020-09-01 20:35:57 +0100

    Updated Js and HTML files to more accurately represent the desired functionality

--- src/main/resources/static /JS/ReadDeletePage.js
+++ src/main/resources/static /JS/ReadDeletePage.js
@@ -5,30 +5,20 @@ function displayWords(){
         if (req.status === 200 && req.readyState === 4) {
             if (req.getResponseHeader("Content-Type") === "application/json") {
                 console.log("oh look its some JSON: " + req.responseText);
-                // adding an element to the body example
-                // let elem = document.createElement('div');
-                // elem.textContent = "hello world";
-                // document.body.appendChild(elem);
-
                 let stuff = JSON.parse(req.response);
                 stuff.forEach(el => {
-                    // console.log(el); // prints whole element
-                    // console.log(el.name); // allows access to specific value
-
-
-                    // adding title to the body of the page
                     let elem = document.createElement('div');
                     let header = document.createElement('h1');
-                    header.textContent = "Notebook name: " + el.name;
+                    header.textContent = "Username: " + el.name;
                     elem.appendChild(header);
-                    el.notes.forEach(note => {
-                        console.log(note) // print all notes for each notebook
-                        let title = document.createElement('p');
-                        let description = document.createElement('p');
-                        title.textContent = "Title: " + note.title;
-                        description.textContent = "Description: " + note.description;
-                        elem.appendChild(title);
-                        elem.appendChild(description);
+                    el.notes.forEach(words => {
+                        console.log(words) // print all notes for each notebook
+                        let word = document.createElement('p');
+                        let definition = document.createElement('p');
+                        word.textContent = "Title: " + words.title;
+                        definition.textContent = "Description: " + words.description;
+                        elem.appendChild(word);
+                        elem.appendChild(definition);
                     })
                     document.body.appendChild(elem);
                 });
